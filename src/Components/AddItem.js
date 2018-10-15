6c9fd8d575829d5d0a26c0ffa8246d2e1f822381
chris <verzetem@gmail.com>
2018-10-15 13:58:59 -0600

    removed a line of code from AddItem.js

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -15,7 +15,6 @@ const AddItem = ({ productsList, addToProdList, inputListen, addToCart }) => {
 						<option id="options" key={product.id} pid={product.id} price={product.priceInCents}>{product.name}</option>
 		      )}
 		    </select>
-		    <h5 className="white">Price per selected item: </h5>
 		  </div>
 		    <button onClick={addToCart} className="btn btn-warning"> Submit </button>
 	  </form>
